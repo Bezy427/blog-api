daf200bfc3523c0589a26b45e686a3ea2dd0d761
Bezy427 <rusamubezel@gmail.com>
2025-12-30 13:08:12 +0200

    clean up

--- src/main/java/com/bezy/blogapi/repositories/TagRepository.java
+++ src/main/java/com/bezy/blogapi/repositories/TagRepository.java
@@ -1,10 +1,7 @@
 package com.bezy.blogapi.repositories;
 
-import com.bezy.blogapi.entities.Post;
 import com.bezy.blogapi.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
